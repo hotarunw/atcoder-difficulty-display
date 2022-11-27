6422d60d23e55c8328d0c0f7348444f7882a508e
hotarunx <29631883+hotarunx@users.noreply.github.com>
2022-11-27 13:10:39 +0900

    [fix]afterbeginか決める属性を追加 処理順を変更して型補完を聞かせる…
    Comfortable AtCoder のドロップダウンが2行にならないように
    FIXME追加

--- src/utils/getElementsColorizable.ts
+++ src/utils/getElementsColorizable.ts
@@ -4,6 +4,7 @@ export type ElementAndId = {
   element: HTMLElement;
   taskID: string;
   big?: boolean;
+  afterbegin?: boolean;
 };
 
 /**
@@ -25,40 +26,43 @@ export const getElementsColorizable = (): ElementAndId[] => {
 
   // aタグ要素 問題ページ、提出ページ等のリンクを想定
   const aTagsRaw = document.getElementsByTagName("a");
-  const aTagsArray = Array.prototype.slice.call(
-    aTagsRaw
-  ) as HTMLAnchorElement[];
-  let aTagsConverted = aTagsArray.map((element) => {
-    const url = parseURL(element.href);
-
-    const taskIDFromURL =
-      url[url.length - 2] === "tasks" ? url[url.length - 1] : "";
-    // 個別の解説ページではbig
-    const big = element.parentElement?.tagName.includes("H2") ?? false;
-    return { element, taskID: taskIDFromURL, big };
-  });
+  let aTagsArray = Array.prototype.slice.call(aTagsRaw) as HTMLAnchorElement[];
   // 問題ページの一番左の要素は除く 見た目の問題です
-  aTagsConverted = aTagsConverted.filter(
+  aTagsArray = aTagsArray.filter(
     (element) =>
       !(
         (pageType === "tasks" || pageType === "score") &&
-        !element.element.parentElement?.previousElementSibling
+        !element.parentElement?.previousElementSibling
       )
   );
 
   // 左上の日本語/英語切り替えリンクは除く
-  aTagsConverted = aTagsConverted.filter(
-    (element) => !element.element.href.includes("?lang=")
-  );
+  aTagsArray = aTagsArray.filter((element) => !element.href.includes("?lang="));
 
   // 解説ページの問題名の右のリンクは除く
-  aTagsConverted = aTagsConverted.filter(
+  aTagsArray = aTagsArray.filter(
     (element) =>
       !(
         pageType === "editorial" &&
-        element.element.children[0]?.classList.contains("glyphicon-new-window")
+        element.children[0]?.classList.contains("glyphicon-new-window")
       )
   );
+
+  const aTagsConverted: ElementAndId[] = aTagsArray.map((element) => {
+    const url = parseURL(element.href);
+
+    const taskIDFromURL =
+      (url[url.length - 2] ?? "") === "tasks" ? url[url.length - 1] ?? "" : "";
+    // 個別の解説ページではbig
+    const big = element.parentElement?.tagName.includes("H2") ?? false;
+
+    // Comfortable AtCoderのドロップダウンではafterbegin
+    const afterbegin =
+      element.parentElement?.parentElement?.classList.contains(
+        "dropdown-menu"
+      ) ?? false;
+    return { element, taskID: taskIDFromURL, big, afterbegin };
+  });
 
   elementsColorizable.push(...aTagsConverted);
 
@@ -67,13 +71,17 @@ export const getElementsColorizable = (): ElementAndId[] => {
   const h3TagsArray = Array.prototype.slice.call(
     h3TagsRaw
   ) as HTMLAnchorElement[];
-  const h3TagsConverted = h3TagsArray.map((element) => {
+  const h3TagsConverted: ElementAndId[] = h3TagsArray.map((element) => {
     const url = parseURL(element.getElementsByTagName("a")[0]?.href ?? "");
 
     const taskIDFromURL =
-      url[url.length - 2] === "tasks" ? url[url.length - 1] : "";
-    return { element, taskID: taskIDFromURL, big: true };
+      (url[url.length - 2] ?? "") === "tasks" ? url[url.length - 1] ?? "" : "";
+    return { element, taskID: taskIDFromURL, big: true, afterbegin: true };
   });
+
+  // FIXME: 別ユーザースクリプトが指定した要素を色付けする機能
+  // 指定したクラスがあれば対象とすることを考えている
+  // ユーザースクリプトの実行順はユーザースクリプトマネージャーの設定で変更可能
 
   elementsColorizable.push(...h3TagsConverted);
 
